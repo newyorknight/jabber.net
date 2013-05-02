fd2ceb6a8024938e0bf057073da966a61f852a8c
Tyler Margion <kolorahl@gmail.com>
2013-05-02 16:15:53 -0700

    Add new collections for better option/property hash management
    - IDefault and IDefault<K> for objects that have "property gets"
    - OptionHash to replace Hashtable and object[][] usage in XmppStream

---
+++ bedrock/collections/IDefault.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Kixeye.Bedrock.Collections
+{
+    /// <summary>
+    /// Represents an object where you can get a property with a default value
+    /// returned if the desired property does not exist or has not been set. It
+    /// is preferred to use the generic IDefault&lt;K&gt; interface when
+    /// possible.
+    /// </summary>
+    public interface IDefault
+    {
+        /// <summary>
+        /// Gets the value of a property with the given key. If the property
+        /// does not exist, or it has not been set, the given default value is
+        /// instead returned.
+        /// </summary>
+        /// <param name="key">The property key.</param>
+        /// <param name="defaultValue">Default value to fallback to.</param>
+        /// <returns>The value of the property or the given default value.</returns>
+        object GetWithDefault(object key, object defaultValue);
+    }
+
+    /// <summary>
+    /// Represents an object where you can get a property with a default value
+    /// returned if the desired property does not exist or has not been set.
+    /// </summary>
+    /// <typeparam name="K">The expected type for property keys.</typeparam>
+    public interface IDefault<K>
+    {
+        /// <summary>
+        /// Gets the value of a property with the given key. If the property
+        /// does not exist, or it has not been set, the given default value is
+        /// instead returned.
+        /// </summary>
+        /// <typeparam name="T">The expected return value type.</typeparam>
+        /// <param name="key">The property key.</param>
+        /// <param name="defaultValue">Default value to fallback to.</param>
+        /// <returns>The value of the property or the given default value.</returns>
+        T GetWithDefault<T>(K key, T defaultValue);
+    }
+}
---
+++ bedrock/collections/OptionHash.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Kixeye.Bedrock.Collections
+{
+    /// <summary>
+    /// A dictionary of String => Object pairs. Contains extension methods to
+    /// the standard set of Dictionary methods.
+    /// </summary>
+    [Serializable]
+    public class OptionHash : Dictionary<string, object>, IDefault<string>
+    {
+        #region Initialization
+        /// <summary>
+        /// Create a new, empty options hash.
+        /// </summary>
+        public OptionHash() : base() { }
+
+        /// <summary>
+        /// Create a new, empty options hash with the starting capacity.
+        /// </summary>
+        /// <param name="capacity">Initial storage capacity.</param>
+        public OptionHash(int capacity) : base(capacity) { }
+
+        /// <summary>
+        /// Creates an option hash from serialization data.
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        protected OptionHash(SerializationInfo info, StreamingContext context) : base(info, context) { }
+        #endregion
+
+
+        #region IDefault
+        /// <summary>
+        /// Gets the value of an option with the given key. If the option
+        /// has not been set, the given default value is returned instead.
+        /// </summary>
+        /// <typeparam name="T">The expected return value type.</typeparam>
+        /// <param name="key">The option key.</param>
+        /// <param name="defaultValue">Default value to fallback to.</param>
+        /// <returns>The value of the option or the given default value.</returns>
+        public T GetWithDefault<T>(string key, T defaultValue)
+        {
+            if (ContainsKey(key))
+            {
+                return (T)this[key];
+            }
+            return defaultValue;
+        }
+        #endregion
+
+
+        #region Merge Hashes
+        /// <summary>
+        /// Merges the other options with this one; this is a destructive
+        /// operation. For a non-destructive alternative, see the class-level
+        /// method OptionHash.Merge(OptionHash, OptionHash). Matching keys from
+        /// the other hash will overwrite values in this one.
+        /// </summary>
+        /// <param name="opts">A hash to merge into this one.</param>
+        public void Merge(OptionHash opts)
+        {
+            IEnumerator<KeyValuePair<string, object>> e = opts.GetEnumerator();
+            while (e.MoveNext())
+            {
+                this[e.Current.Key] = e.Current.Value;
+            }
+        }
+
+        /// <summary>
+        /// Merges two option hashes into one. This is a non-destructive
+        /// operation, meaning the original two hashes will not be modified.
+        /// Matching keys will use the value from the second hash as the final
+        /// value.
+        /// </summary>
+        /// <param name="one">Hash to merge.</param>
+        /// <param name="two">Hash to merge.</param>
+        /// <returns>A new OptionHash object.</returns>
+        public static OptionHash Merge(OptionHash one, OptionHash two)
+        {
+            OptionHash merge = new OptionHash(Math.Max(one.Count, two.Count));
+            // TODO: Optimize this?
+            merge.Merge(one);
+            merge.Merge(two);
+            return merge;
+        }
+        #endregion
+    }
+}
